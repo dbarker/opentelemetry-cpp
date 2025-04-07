1a084f16a87f590ec66f4223ff0ba2c1ffafaed4
Douglas Barker <doug.barker@gmail.com>
2025-04-07 17:00:37 -0600

    add custom debug only triplet for windows vcpkg builds. Use this in the setup windows ci environment script. Disable unused grpc plugins

--- ci/setup_windows_ci_environment.ps1
+++ ci/setup_windows_ci_environment.ps1
@@ -7,25 +7,39 @@ trap { $host.SetShouldExit(1) }
 git submodule update -f "tools/vcpkg"
 Push-Location -Path "tools/vcpkg"
 $VCPKG_DIR = (Get-Item -Path ".\").FullName
+
+$TripletsDir = Join-Path $VCPKG_DIR "..\triplets"
+$TripletOptions = "--overlay-triplets=$TripletsDir --triplet=x64-windows-debug-only"
+
 ./bootstrap-vcpkg.bat
 ./vcpkg integrate install
 
 # Google Benchmark
-./vcpkg "--vcpkg-root=$VCPKG_DIR" install benchmark:x64-windows
+./vcpkg "--vcpkg-root=$VCPKG_DIR" install benchmark:x64-windows $TripletOptions
 
 # Google Test
-./vcpkg "--vcpkg-root=$VCPKG_DIR" install gtest:x64-windows
+./vcpkg "--vcpkg-root=$VCPKG_DIR" install gtest:x64-windows $TripletOptions
 
 # nlohmann-json
-./vcpkg "--vcpkg-root=$VCPKG_DIR" install nlohmann-json:x64-windows
+./vcpkg "--vcpkg-root=$VCPKG_DIR" install nlohmann-json:x64-windows $TripletOptions
 
-# grpc
-./vcpkg "--vcpkg-root=$VCPKG_DIR" install grpc:x64-windows
+# grpc with custom CMake arguments
+./vcpkg "--vcpkg-root=$VCPKG_DIR" install grpc:x64-windows $TripletOptions `
+  --x-cmake-args=" -DgRPC_INSTALL=ON `
+    -DgRPC_BUILD_TESTS=OFF `
+    -DgRPC_BUILD_GRPC_CPP_PLUGIN=ON `
+    -DgRPC_BUILD_GRPC_CSHARP_PLUGIN=OFF `
+    -DgRPC_BUILD_GRPC_OBJECTIVE_C_PLUGIN=OFF `
+    -DgRPC_BUILD_GRPC_PHP_PLUGIN=OFF `
+    -DgRPC_BUILD_GRPC_NODE_PLUGIN=OFF `
+    -DgRPC_BUILD_GRPC_PYTHON_PLUGIN=OFF `
+    -DgRPC_BUILD_GRPC_RUBY_PLUGIN=OFF `
+    -DgRPC_BUILD_GRPCPP_OTEL_PLUGIN=OFF"
 
 # curl
-./vcpkg "--vcpkg-root=$VCPKG_DIR" install curl:x64-windows
+./vcpkg "--vcpkg-root=$VCPKG_DIR" install curl:x64-windows $TripletOptions
 
 # prometheus-cpp
-./vcpkg "--vcpkg-root=$VCPKG_DIR" install prometheus-cpp:x64-windows
+./vcpkg "--vcpkg-root=$VCPKG_DIR" install prometheus-cpp:x64-windows $TripletOptions
 
 Pop-Location
---
+++ tools/triplets/x64-windows-debug-only.cmake
@@ -0,0 +1,7 @@
+# Copyright The OpenTelemetry Authors
+# SPDX-License-Identifier: Apache-2.0
+
+set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE dynamic)
+set(VCPKG_BUILD_TYPE debug)
